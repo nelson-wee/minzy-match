682bdff0280dafa035765258a0d281e3e1d23cd4
nelson-wee <nelson-wee@users.noreply.github.com>
2026-05-05 01:53:16 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppRoot from './App.jsx'
+import AppRoot from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRoot />
   </React.StrictMode>,
